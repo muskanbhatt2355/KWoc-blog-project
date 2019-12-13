8cfa84a3d5908a67da77d9d191b5fb438e466828
Muskan <muskanbhatt2355@gmail.com>
2019-12-13 20:14:14 +0530

    KWoC blog-project

---
+++ blog/views.py
@@ -0,0 +1,164 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from blog.models import Post, Comment
+from django.utils import timezone
+from blog.forms import PostForm, CommentForm
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib.auth import authenticate,login,logout
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from django.views.generic import (TemplateView,ListView,
+                                  DetailView,CreateView,
+                                  UpdateView,DeleteView)
+
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+
+    form_class = PostForm
+
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+
+    form_class = PostForm
+
+    model = Post
+
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+def signup(request):
+    if request.method == "POST":
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.filter(username = request.POST['username'])
+            if user:
+                return render(request,'signup.html',{'error' : '*Username already exist'})
+            else:
+                user = User.objects.create_user(username = request.POST['username'],password = request.POST['password1'])
+                auth.login(request,user,backend='django.contrib.auth.backends.ModelBackend')
+                return render(request,'post_list.html')
+        else:
+            return render(request,'signup.html',{'error':'*Passwords must match'})
+    else:
+        return render(request,'signup.html')
+
+def passwordreset(request):
+    if request.method == "POST":
+        if request.POST['password1'] == request.POST['password2']:
+            u = User.objects.get(username__exact=request.user.username)
+            u.set_password(request.POST['password1'])
+            u.save()
+            return render(request,'post_list.html')
+
+        else:
+            return render(request,'passwordreset.html',{'error':'*Passwords must match'})
+
+    else:
+        return render(request,'passwordreset.html')
+
+
+
+#######################################
+## Functions that require a pk match ##
+#######################################
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form': form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
+
+def user_login(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username,password=password)
+        if user:
+            if user.is_active:
+
+                login(request,user)
+                request.session['member_name'] = username
+                print("Hello World Done")
+                return HttpResponseRedirect(reverse('dashboard'))
+
+    return render(request,'registration/user_login.html')
+
+def dashboard(request):
+    return render(request,'registration/dashboard.html')
+
+@login_required
+def userdetials(request,pk):
+    if request.method == "POST":
+        user = User.objects.get(pk=pk)
+        user.profile.photo = request.FILES['photo']
+        user.save()
+        return render(request,'blog/post_list.html')
+    else:
+        user = get_object_or_404(User,id=pk)
+        return render(request,'blog/user_details.html',{'user':user})
